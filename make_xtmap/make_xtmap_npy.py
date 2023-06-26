correcting the xt_map code (averaging not correct)

make_xtmap/make_xtmap_npy.py
@@ -25,6 +25,7 @@ import numpy as np
 import glob
 import pandas as pd
 from scipy.interpolate import interp1d
+import os
 
 def apply_slits(slit,data):
     y,x=np.split(slit,2,axis=1)
@@ -32,15 +33,15 @@ def apply_slits(slit,data):
     y1=y+1
     x_1=x-1
     y_1=y-1
-    sl_1=np.column_stack((x1,y1))
-    sl_2=np.column_stack((x_1,y_1))
-    sl_3=np.column_stack((x_1,y1))
-    sl_4=np.column_stack((x1,y_1))
-    pix0=[]
-    pix1=[]
-    pix2=[]
-    pix3=[]
-    pix4=[]
+    # sl_1=np.column_stack((x1,y1))
+    # sl_2=np.column_stack((x_1,y_1))
+    # sl_3=np.column_stack((x_1,y1))
+    # sl_4=np.column_stack((x1,y_1))
+    # pix0=[]
+    # pix1=[]
+    # pix2=[]
+    # pix3=[]
+    # pix4=[]
     pix0=data[x,y].T[0]
     pix1=data[x1,y1].T[0]
     pix2=data[x_1,y_1].T[0]
@@ -52,6 +53,8 @@ def apply_slits(slit,data):
     inter_slit_4=interp1d(pix_array,pix3,kind='cubic')
     inter_slit_5=interp1d(pix_array,pix4,kind='cubic')
     return((inter_slit_1(pix_array)+inter_slit_2(pix_array)+inter_slit_3(pix_array)+inter_slit_4(pix_array)+inter_slit_5(pix_array))/5)
+    # return((inter_slit_1(pix_array)))
+    # return(pix0)
 
 folder_uni=input("Enter path of the folder containing datasets and slits: ")+"/"
 slit_folder_uni=folder_uni+"Slits"+"/"
@@ -75,8 +78,12 @@ for slit_folder in sorted(glob.glob(slit_folder_uni+"S*")):
         data=read_file['data']
         for slit in slits:
             arr+=apply_slits(slit,data) 
-            arr=arr/len(slits)
+        arr=arr/len(slits)
         xt_map.append(arr)
 
     xt_map=np.array(xt_map)
     np.savetxt(info_data[0,6]+"xt_map.csv",xt_map,delimiter=',')
+    cfg_file=folder_in+"info/units.cfg"
+    cfg_file_cp=info_data[0,6]+"units.cfg"
+    cmd=f"cp {cfg_file} {cfg_file_cp}"
+    os.system(cmd)